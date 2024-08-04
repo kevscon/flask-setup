acb6484e80b60c3a6411958f477b8506e658acfa
kevscon <kevscon@gmail.com>
2024-08-04 18:16:18 -0400

    data

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    input_message = 'Hello, World!!!'
-    return jsonify({'message': input_message})
+@app.route('/api', methods=['POST'])
+def submit_text():
+    input_data = request.json
+    text_input = input_data.get('text', '')
+    return jsonify({'message': text_input + '!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
