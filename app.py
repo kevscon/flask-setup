829331aa9c3b4ea6b6cdd07000c4f3d0eb9e1161
kevscon <kevscon@gmail.com>
2024-08-05 19:29:27 -0400

    reverse text

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api', methods=['POST'])
-def submit_text():
-    input_data = request.json
-    text_input = input_data.get('text', '')
-    return jsonify({'message': text_input + '!'})
+@app.route('/reverse', methods=['POST'])
+def reverse_text():
+    data = request.json
+    text = data.get('text', '')
+    reversed_text = text[::-1]
+    return jsonify({'reversed_text': reversed_text})
 
 if __name__ == '__main__':
     app.run(debug=True)
